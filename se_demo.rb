ca80e546e4860b480192f8ad22d8ac83db9d01dc
Jeremy Evans <code@jeremyevans.net>
2008-03-16 17:56:05 +0000

    Add se_demo.rb and related files, for running a demo site for scaffolding extensions

---
+++ se_demo.rb
@@ -0,0 +1,13 @@
+require 'rubygems'
+require 'ramaze'
+SE_TEST_FRAMEWORK='se_demo'
+require 'sequel_setup'
+require 'se_setup'
+ScaffoldingExtensions.javascript_library = 'JQuery'
+
+class MainController < Ramaze::Controller
+  layout :se_layout
+  scaffold_all_models :only=>[SqEmployee, SqGroup, SqMeeting, SqOfficer, SqPosition]
+end
+
+Ramaze.start :adapter=>:mongrel, :port=>7980, :sourcereload=>false
