fd6358f167db79efb6b51b689d6a7c8a88ca85c1
Jeremy Evans <code@jeremyevans.net>
2008-04-07 10:20:09 -0700

    Better .gitignore file

---
+++ test_site/bootstrap.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+FRAMEWORKS=%w'ramaze camping sinatra'
+ORMS=%w'active_record data_mapper sequel'
+SE_TEST_FRAMEWORK='rails'
+# Make log and tmp directories
+system 'mkdir -p log tmp tmp/{sessions,sockets,cache,pids}'
+# Make sure log files exist to shut up Rails
+system 'touch log/{development,production,test}.log'
+# Create the Rails databases
+system 'rake db:create'
+system 'rake db:migrate'
+require 'data_mapper_setup'
+DataMapper::Persistence.auto_migrate!
+require 'sequel_setup'
+Sequel::Migrator.apply(SequelDB, 'db/sequel_migrate')
+# Copy the Rails databases to the other frameworks
+FRAMEWORKS.each{|f| ORMS.each{|o| system "cp db/#{SE_TEST_FRAMEWORK}.#{o}.sqlite3 db/#{f}.#{o}.sqlite3"}}
