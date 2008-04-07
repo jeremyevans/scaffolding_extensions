a29dd77be7601e31b46013019c718ad340e17a45
Jeremy Evans <code@jeremyevans.net>
2008-04-07 10:22:49 -0700

    Don't add data_mapper/lib to the load path in the test site, now that 0.3.0 works

--- test_site/data_mapper_setup.rb
+++ test_site/data_mapper_setup.rb
@@ -1,4 +1,3 @@
-$:.unshift("data_mapper/lib")
 require 'data_mapper'
 DataMapper::Database.setup(:adapter=>:sqlite3, :database=>"db/#{SE_TEST_FRAMEWORK}.data_mapper.sqlite3")
 # DATAMAPPER_BUG: Must have models for HABTM join tables
@@ -6,4 +5,3 @@ DataMapper::Database.setup(:adapter=>:sqlite3, :database=>"db/#{SE_TEST_FRAMEWOR
 %w'dm_employee dm_group dm_position dm_officer dm_meeting'.each{|x| require "model/#{x}"}
 #DataMapper.database.logger.level = 0
 #DataMapper.database.logger.instance_variable_set(:@logdev, STDOUT)
-
