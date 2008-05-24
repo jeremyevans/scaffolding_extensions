fc5f61b0308fe1eaea1bc9ee84111a76f416c31e
Jeremy Evans <code@jeremyevans.net>
2008-05-23 20:55:29 -0700

    Turn off typecasting to work with the master branch of Sequel (requires Sequel master/2.0)

--- test_site/sequel_setup.rb
+++ test_site/sequel_setup.rb
@@ -1,5 +1,6 @@
 require 'sequel'
 SequelDB = Sequel.sqlite("db/#{SE_TEST_FRAMEWORK}.sequel.sqlite3")
+Sequel::Model.typecast_on_assignment = false
 %w'sq_employee sq_group sq_position sq_officer sq_meeting'.each{|x| require "model/#{x}"}
 require 'logger'
 #SequelDB.logger = Logger.new(STDOUT)
