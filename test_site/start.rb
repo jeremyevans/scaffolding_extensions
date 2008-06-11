ffc2cfc6b6e2d779d08d60d435880ccbbe734db5
Jeremy Evans <code@jeremyevans.net>
2008-06-11 12:16:58 -0700

    Support Ramaze 2008.06

--- test_site/start.rb
+++ test_site/start.rb
@@ -1,6 +1,10 @@
+#!/usr/bin/env ruby
 require 'rubygems'
 require 'ramaze'
 SE_TEST_FRAMEWORK='ramaze'
+class String
+  undef_method :start_with?
+end
 require 'active_record_setup'
 require 'data_mapper_setup'
 require 'sequel_setup'
@@ -8,4 +12,6 @@ require 'se_setup'
 require 'controller/main'
 
 #Ramaze::Inform.loggers = []
-Ramaze.start :adapter => :mongrel, :port => 7978
+Ramaze::Global.adapter = :mongrel
+Ramaze::Global.port = 7978
+#Ramaze.start
