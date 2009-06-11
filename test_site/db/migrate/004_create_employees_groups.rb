a393af288b589c5e9381e7a49205bd46ee339392
Michael Fellinger <m.fellinger@gmail.com>
2009-06-12 00:46:55 +0900

    Fix Syntax issues with Ruby 1.9

--- test_site/db/migrate/004_create_employees_groups.rb
+++ test_site/db/migrate/004_create_employees_groups.rb
@@ -1,6 +1,6 @@
 class CreateEmployeesGroups < ActiveRecord::Migration
   def self.up
-    create_table (:employees_groups, :id=>false) do |t|
+    create_table :employees_groups, :id => false do |t|
       t.integer :employee_id
       t.integer :group_id
     end
