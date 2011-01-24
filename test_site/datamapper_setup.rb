df594336f9a0a9816956fd72ffa77bb56cdb4267
Jeremy Evans <code@jeremyevans.net>
2011-01-24 14:21:10 -0800

    Use URL for setting up datamapper, the hash syntax doesn't seem to work in 1.0.2 on ruby 1.9

--- test_site/datamapper_setup.rb
+++ test_site/datamapper_setup.rb
@@ -3,6 +3,6 @@ require "dm-validations"
 require "dm-transactions"
 require "data_objects"
 require "do_sqlite3"
-DataMapper.setup(:default, {:adapter => "sqlite3", :database => "db/#{SE_TEST_FRAMEWORK}.datamapper.sqlite3" })
+DataMapper.setup(:default, "sqlite3:db/#{SE_TEST_FRAMEWORK}.datamapper.sqlite3")
 %w'dm_employee dm_group dm_position dm_officer dm_meeting'.each{|x| require "model/#{x}"}
 require 'model/dm_resources'
