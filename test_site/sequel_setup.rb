117ff79716635984d70ec227f02d8fb389982a6f
Jeremy Evans <code@jeremyevans.net>
2011-07-03 17:14:13 -0700

    Use the Sequel prepared statement plugins in the test site

--- test_site/sequel_setup.rb
+++ test_site/sequel_setup.rb
@@ -2,8 +2,10 @@ $:.unshift('/data/code/sequel/lib')
 require 'sequel'
 Sequel.single_threaded = true
 SequelDB = Sequel.sqlite("db/#{SE_TEST_FRAMEWORK}.sequel.sqlite3", :single_threaded=>true, :foreign_keys=>false)
+Sequel::Model.plugin :prepared_statements
+Sequel::Model.plugin :prepared_statements_associations
 %w'sq_employee sq_group sq_position sq_officer sq_meeting'.each{|x| require "model/#{x}"}
 require 'logger'
-#SequelDB.logger = Logger.new(STDOUT)
+SequelDB.logger = Logger.new(STDOUT)
 require 'sequel/extensions/inflector'
 require 'sequel/extensions/migration'
