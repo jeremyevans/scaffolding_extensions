3bbdb0d56f764a6617bb21ac2218edc41f2ccd3e
Jeremy Evans <code@jeremyevans.net>
2012-04-27 12:29:17 -0700

    Don't require the Sequel core extensions in the test site
    ScaffoldingExtensions hasn't depended on them, but it's best
    to ensure they aren't used accidently.

--- test_site/sequel_setup.rb
+++ test_site/sequel_setup.rb
@@ -1,7 +1,7 @@
 $:.unshift('/data/code/sequel/lib')
-require 'sequel'
+require 'sequel/no_core_ext'
 Sequel.single_threaded = true
-SequelDB = Sequel.sqlite("db/#{SE_TEST_FRAMEWORK}.sequel.sqlite3", :single_threaded=>true, :foreign_keys=>false)
+SequelDB = Sequel.sqlite("db/#{SE_TEST_FRAMEWORK}.sequel.sqlite3", :foreign_keys=>false)
 Sequel::Model.plugin :prepared_statements
 Sequel::Model.plugin :prepared_statements_associations
 %w'sq_employee sq_group sq_position sq_officer sq_meeting'.each{|x| require "model/#{x}"}
