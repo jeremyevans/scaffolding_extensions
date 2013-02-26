4be7eeb1b3024597026bb1001a5321e14b597a10
Jeremy Evans <code@jeremyevans.net>
2013-02-26 14:09:23 -0800

    Add a Gemfile for test site for easier bootstrapping, but don't use it when testing
    Upgrade to Rails 3.2.12 while here.

--- test_site/config/boot.rb
+++ test_site/config/boot.rb
@@ -1,7 +1,7 @@
 require 'rubygems'
-gem 'rails', '3.2.11'
+gem 'rails', '3.2.12'
 
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
-require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+#require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
